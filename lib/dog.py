1f3cdfba83abfa9524737d53d25f8d0cfdf0ae95
George <gnm7208@gmail.com>
2025-12-01 01:01:32 +0300

    Implement update_breed function

--- lib/dog.py
+++ lib/dog.py
@@ -20,4 +20,5 @@ def find_by_name_and_breed(session, name, breed):
     return session.query(Dog).filter_by(name=name, breed=breed).first()
 
 def update_breed(session, dog, breed):
-    pass
+    dog.breed = breed
+    session.commit()

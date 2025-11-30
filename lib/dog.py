523879fa7c5f5be1a4d91c33df5922610b319cf4
George <gnm7208@gmail.com>
2025-12-01 01:00:58 +0300

    Implement find_by_id function

--- lib/dog.py
+++ lib/dog.py
@@ -14,7 +14,7 @@ def find_by_name(session, name):
     return session.query(Dog).filter_by(name=name).first()
 
 def find_by_id(session, id):
-    pass
+    return session.query(Dog).filter_by(id=id).first()
 
 def find_by_name_and_breed(session, name, breed):
     pass

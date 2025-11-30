208246347274fe44a4b30a0c5475784cb4e1027e
George <gnm7208@gmail.com>
2025-12-01 01:02:18 +0300

    Add docstrings to find_by_name and find_by_id functions

--- lib/dog.py
+++ lib/dog.py
@@ -14,9 +14,11 @@ def get_all(session):
     return session.query(Dog).all()
 
 def find_by_name(session, name):
+    """Find a dog by name."""
     return session.query(Dog).filter_by(name=name).first()
 
 def find_by_id(session, id):
+    """Find a dog by id."""
     return session.query(Dog).filter_by(id=id).first()
 
 def find_by_name_and_breed(session, name, breed):
